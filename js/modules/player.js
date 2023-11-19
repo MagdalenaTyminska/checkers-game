710fedeab30a3e392552734d2ce730c70a6ee8c9
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-11-19 22:21:39 +0100

    implement player moves

--- js/modules/player.js
+++ js/modules/player.js
@@ -1,16 +1,25 @@
 export class Player {
   #name;
+  #score = 0;
 
   constructor(name) {
-      this.name = name;
+    this.name = name;
   }
 
   // metody/właściwości dostępowe
   get name() {
-      return this.#name;
+    return this.#name;
   }
 
   set name(value) {
-      this.#name = value;
+    this.#name = value;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  set score(value) {
+    this.#score = value;
   }
 }
